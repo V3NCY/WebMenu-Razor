f605f8cb1bb812f1d9e731b2919bb57e75eae792
unknown <venceslavam@gmail.com>
2022-11-04 15:34:13 +0200

    cart new fix

---
+++ WebMenu-Razor/Pages/Cart.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMenu_Razor.Pages
+{
+    public class CartModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
